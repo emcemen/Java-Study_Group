aed45132d0d794e04ffff2caa6a08510bba88d2f
Emre YILMAZ <107490353+emcemen@users.noreply.github.com>
2024-02-19 13:44:23 -0500

    Day4-Java examples

---
+++ src/test/java/Day4/ReverseString.java
@@ -0,0 +1,13 @@
+package Day4;
+
+public class ReverseString {
+    public static void main(String[]Args){
+
+        String word ="Study Group";
+
+        StringBuilder reversedString= new StringBuilder(word).reverse();
+        System.out.println(word.toString());
+
+    }
+
+}
